32ce87ee076cda31fb8c1183465d6121bb71e081
Magomed Khadzhiev <tyrneni@yandex.ru>
2022-02-02 14:15:12 +0300

    1 modul

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 /**
  * omit - creates an object composed of enumerable property fields
  * @param {object} obj - the source object
@@ -5,5 +6,14 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+    let sortedObj = [];
+
+    Object.entries(obj).forEach((key, index) => {
+        if (!fields.includes(key[0])) {
+            sortedObj.push(Object.entries(obj)[index]);
+        }
+    });
+    
+    return Object.fromEntries(sortedObj);
 
 };
